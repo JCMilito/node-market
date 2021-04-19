f25f95fdeec06673db9cd44c57e0f61e99844ebe
JCMilito <jcm2112@gmail.com>
2021-04-19 10:58:04 -0300

    Yo

--- src/DAO.ts
+++ src/DAO.ts
@@ -1,41 +1,45 @@
-import lowdb from 'lowdb';
-import FileSync from 'lowdb/adapters/FileSync';
-import { sortByName } from './util';
-import { nanoid } from 'nanoid';
-import { Product } from './model/Product';
+import lowdb from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+import { nanoid } from "nanoid";
+import { Product } from "./model/Product";
 
 export default class DAO {
+  db = lowdb(new FileSync("db.json"));
 
-    db = lowdb(new FileSync('db.json'));
+  constructor() {
+    this.db.defaults({ products: [] }).write();
+  }
 
-    constructor() {
-        this.db.defaults({ products: [] }).write();
-    }
+  listProducts(): Product[] {
+    return sortByName(this.db.get("products").value());
+  }
 
-    listProducts(): Product[] {
-        return sortByName(this.db.get('products').value()); 
-    }
+  findProduct(id: string): Product {
+    // @ts-ignore
+    return this.db.get("products").find({ id }).value();
+  }
 
-    findProduct(id: string): Product {
-        // @ts-ignore        
-        return this.db.get('products').find({ id }).value();
-    }    
+  createProduct(data: any): void {
+    // @ts-ignore
+    this.db.get("products").push(new Product(nanoid(), data.name, data.price))
+      .write();
+  }
 
-    createProduct(data: any): void {
-        // @ts-ignore        
-        this.db.get('products').push(new Product(nanoid(), data.name, data.price)).write();        
-    }
+  updateProduct(product: Product): void {
+    // @ts-ignore
+    this.db.get("products").find({ id: product.id })
+      .assign({ name: product.name, price: product.price })
+      .write();
+  }
 
-    updateProduct(product: Product): void {
-        // @ts-ignore
-        this.db.get('products').find({ id: product.id }).
-        assign({ name: product.name, price: product.price }).write();
-    }
-
-    deleteProduct(id: string): void {
-        // @ts-ignore
-        this.db.get('products').remove({ id: id }).write();
-    }
+  deleteProduct(id: string): void {
+    // @ts-ignore
+    this.db.get("products").remove({ id: id }).write();
+  }
 }
 
-
+function sortByName(products: Product[]): Product[] {
+  return products.sort((a: Product, b: Product) =>
+    a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+  );
+}
--- src/Util.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from './model/Product';
-
-export function sortByName(products: Product[]): Product[] {
-    return products.sort((a: Product, b: Product) => 
-        (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
-}
